347b6e89b3dbac3e3e4341a7dcb01c32ea1b9f4f
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2023-01-22 21:57:23 +0700

    git commit init

---
+++ src/array/Test.java
@@ -0,0 +1,45 @@
+package array;
+
+public class Test {
+    public static void main(String[] args) {
+        DynamicArray<String> dynamicArray = new DynamicArray<>();
+//        dynamicArray.add("O");
+//        dynamicArray.add("n");
+//        dynamicArray.add("g");
+//        dynamicArray.add("d");
+//        dynamicArray.add("e");
+//        dynamicArray.add("V");
+//        dynamicArray.add("E");
+//        dynamicArray.add("P");
+
+//        System.out.println(dynamicArray.toString());
+//        System.out.println(dynamicArray.get(2));
+//        System.out.println(dynamicArray.indexOf("0"));
+//        dynamicArray.removeAt(4);
+//        System.out.println(dynamicArray.toString());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+        dynamicArray.add("O");
+        System.out.println(dynamicArray.size());
+    }
+}
