23d3cc24af4876d52af4369e730265020deea6c2
bangnguyen123 <comtrang.gioson.giolinh@gmail.com>
2023-12-25 19:35:53 +0700

    add head and tail for list

--- src/linkedlist/golang/linkedlist.go
+++ src/linkedlist/golang/linkedlist.go
@@ -11,7 +11,7 @@ type LinkedList struct {
 	headNode *Node
 }
 
-func (linkedList *LinkedList) addToHead(property int) {
+func (linkedList *LinkedList) AddToHead(property int) {
 	var node = Node{}
 	node.property = property
 	if node.nextNode != nil {
@@ -20,9 +20,37 @@ func (linkedList *LinkedList) addToHead(property int) {
 	linkedList.headNode = &node
 }
 
+func (linkedList *LinkedList) IterateList() {
+	for node := linkedList.headNode; node != nil; node = node.nextNode {
+		fmt.Println(node.property)
+	}
+}
+
+func (linkedList *LinkedList) Tail() *Node {
+	var tail *Node
+	var node *Node
+	for node = linkedList.headNode; node != nil; node = node.nextNode {
+		if node.nextNode == nil {
+			tail = node
+		}
+	}
+	return tail
+}
+
+func (linkedList LinkedList) AddTail(property int) {
+	node := &Node{}
+	node.property = property
+	node.nextNode = nil
+	tailNode := linkedList.Tail()
+	if tailNode != nil {
+		tailNode.nextNode = node
+	}
+}
+
 func main() {
 	linkedList := LinkedList{}
-	linkedList.addToHead(1)
-	linkedList.addToHead(3)
+	linkedList.AddToHead(1)
+	linkedList.AddToHead(3)
+	fmt.Print(linkedList.Tail())
 	fmt.Println(linkedList.headNode.property)
 }
